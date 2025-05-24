392d1c0ea7462b452074aa57379e46514fce46a3
J M Archer <archer884@zoho.com>
2025-05-24 13:10:19 -0500

    I'm going to have to rewrite this from scratch. Jesus H. Christ...

--- src/lib.rs
+++ src/lib.rs
@@ -1,74 +1,29 @@
+use punkt::{params::Standard, SentenceTokenizer, Trainer, TrainingData};
+use regex::Regex;
+use unicode_segmentation::UnicodeSegmentation;
 use wordsworth::syllable_counter;
 
-use punkt::params::Standard;
-use punkt::SentenceTokenizer;
-use punkt::Trainer;
-use punkt::TrainingData;
-
-use unicode_segmentation::UnicodeSegmentation;
-
-use regex::Regex;
-
-use std::fs;
-
-pub fn word_list_from_string(string_to_analyze: &str) -> Vec<String> {
-    let mut word_list: Vec<String> = Vec::new();
-    let words = string_to_analyze.unicode_words();
-    for w in words {
-        word_list.push(w.to_string());
-    }
-
-    word_list
+fn word_list_from_string(s: &str) -> Vec<String> {
+    s.unicode_words().map(|s| s.to_owned()).collect()
 }
 
-pub fn word_list_from_file(filename: &str) -> Vec<String> {
-    let mut word_list: Vec<String> = Vec::new();
-    let string_from_file = fs::read_to_string(filename).expect("Unable to read file");
-    let words = string_from_file.unicode_words();
-    for w in words {
-        word_list.push(w.to_string());
-    }
-    for s in &word_list {
-        println!("{}", s);
-    }
-    word_list
-}
-
-pub fn file_to_string(filename: &str) -> String {
-    let string_from_file = fs::read_to_string(filename).expect("Unable to read file");
-    string_from_file
-}
-
-pub fn long_words(word_list: Vec<String>) -> f64 {
-    let mut sum = 0;
-    for word in word_list {
-        if word.len() >= 6 {
-            sum += 1;
-        }
-    }
-    let long_words_count: f64;
-    long_words_count = sum as f64;
-    long_words_count
+pub fn long_words(word_list: &[String]) -> f64 {
+    word_list.iter().filter(|&s| s.len() >= 6).count() as f64
 }
 
 pub fn percent_long_words(word_list: Vec<String>) -> f64 {
     let list_count = word_list.len() as i32;
-    let long_words_count = long_words(word_list);
-    let percent_longwords = long_words_count as f64 / list_count as f64;
-    percent_longwords
+    let long_words_count = long_words(&word_list);
+    long_words_count / list_count as f64
 }
 
 pub fn character_count(string_to_analyze: &str) -> f64 {
     let re = Regex::new(r"[^\w]").unwrap();
     let result = re.replace_all(string_to_analyze, "");
-
-    let character_count: f64;
-    character_count = result.graphemes(true).count() as f64;
-
-    character_count
+    result.graphemes(true).count() as f64
 }
 
-pub fn linsear_scoring(sylcount_list: Vec<i32>) -> f64{
+pub fn linsear_scoring(sylcount_list: Vec<i32>) -> f64 {
     let mut score_sum: f64;
     score_sum = 0.0;
     for syl in &sylcount_list {
@@ -84,7 +39,7 @@ pub fn linsear_scoring(sylcount_list: Vec<i32>) -> f64{
 pub fn syllable_count_list(word_list: Vec<String>) -> Vec<i32> {
     let mut sylcount_list: Vec<i32> = Vec::new();
     for word in &word_list {
-        sylcount_list.push(syllable_counter(&word).try_into().unwrap());
+        sylcount_list.push(syllable_counter(word).try_into().unwrap());
     }
     sylcount_list
 }
@@ -94,9 +49,7 @@ pub fn avg_syl_count(sylcount_list: Vec<i32>) -> f64 {
     for w in &sylcount_list {
         sum += w;
     }
-    let avg_syls: f64;
-    avg_syls = sum as f64 / sylcount_list.len() as f64;
-    avg_syls
+    sum as f64 / sylcount_list.len() as f64
 }
 
 // num of words with 2 syllables or less;
@@ -124,9 +77,7 @@ pub fn long_syl_count(word_list: Vec<String>) -> i32 {
 }
 
 pub fn wordcount_list(word_list: Vec<String>) -> i32 {
-    let wordcount: i32;
-    wordcount = word_list.len() as i32;
-    wordcount
+    word_list.len() as i32
 }
 
 pub fn split_into_sentences(doc: &str) -> Vec<String> {
@@ -164,199 +115,72 @@ pub fn sent_word_counts(doc: &str) -> Vec<usize> {
     sent_wordcount_list
 }
 
-pub fn sent_avg_wordcount(sent_wordcount_list: Vec<usize>) -> f64 {
+pub fn sentence_average_word_count(sent_wordcount_list: Vec<usize>) -> f64 {
     let mut sum = 0;
     for w in &sent_wordcount_list {
         sum += w;
     }
-    let avg_word_length: f64;
-    avg_word_length = sum as f64 / sent_wordcount_list.len() as f64;
-    avg_word_length
+    sum as f64 / sent_wordcount_list.len() as f64
 }
 
-pub fn lix(file_to_analyze: &str) -> f64 {
-    //file to string
-    let string_to_analyze = file_to_string(file_to_analyze);
-    //total_words
-    let all_words = word_list_from_string(&string_to_analyze);
-    //num long words
-    let num_long_words = percent_long_words(all_words);
-    //avg_num_words/sentence
-    let sent_wordcount_list = sent_word_counts(&string_to_analyze);
-    let avg_words_per_sentence = sent_avg_wordcount(sent_wordcount_list);
-    let lix_index = num_long_words + avg_words_per_sentence;
-    println!("{}", lix_index);
-    lix_index
-}
-
-pub fn rix(file_to_analyze: &str) -> f64 {
-    //file to string
-    let string_to_analyze = file_to_string(file_to_analyze);
-    //total_words
-    let all_words = word_list_from_string(&string_to_analyze);
-    //num long words
-    let num_long_words = long_words(all_words);
-    let num_of_sents = split_into_sentences(&string_to_analyze).len() as f64;
-    let rix_index = num_long_words / num_of_sents;
-    println!("{}", rix_index);
-    rix_index
-}
-
-pub fn flesch(file_to_analyze: &str) -> f64 {
-    //file to string
-    let string_to_analyze = file_to_string(file_to_analyze);
-    let sent_counts = sent_word_counts(&string_to_analyze);
-    let avg_sent_length = sent_avg_wordcount(sent_counts);
+pub fn flesch_reading_ease(string_to_analyze: &str) -> f64 {
+    let sent_counts = sent_word_counts(string_to_analyze);
+    let avg_sent_length = sentence_average_word_count(sent_counts);
     //syls
-    let all_words = word_list_from_string(&string_to_analyze);
+    let all_words = word_list_from_string(string_to_analyze);
     let all_syls = syllable_count_list(all_words);
     let avg_syls = avg_syl_count(all_syls);
-    let flesch_index = 206.835 - (1.015 * avg_sent_length) - (84.6 * avg_syls);
-    println!("{}", flesch_index);
-    flesch_index
+    206.835 - (1.015 * avg_sent_length) - (84.6 * avg_syls)
 }
 
-pub fn flesch_kincaid(file_to_analyze: &str) -> f64 {
-    //file to string
-    let string_to_analyze = file_to_string(file_to_analyze);
+pub fn flesch_kincaid_grade_level(string_to_analyze: &str) -> f64 {
     // avg_words_per_sentence
-    let sent_counts = sent_word_counts(&string_to_analyze);
-    let avg_sent_length = sent_avg_wordcount(sent_counts);
+    let sent_counts = sent_word_counts(string_to_analyze);
+    let avg_sent_length = sentence_average_word_count(sent_counts);
     // avg syls per word
-    let all_words = word_list_from_string(&string_to_analyze);
+    let all_words = word_list_from_string(string_to_analyze);
     let all_syls = syllable_count_list(all_words);
     let avg_syls = avg_syl_count(all_syls);
-    let flesch_kincaid_index = (0.39 * avg_sent_length) + (11.8 * avg_syls) - 15.59;
-    println!("{}", flesch_kincaid_index);
-    flesch_kincaid_index
+    (0.39 * avg_sent_length) + (11.8 * avg_syls) - 15.59
 }
 
-pub fn linsear_write(file_to_analyze: &str) -> f64{
-    //file to string
-    let string_to_analyze = file_to_string(file_to_analyze);
-    //compiling variables for linsear_write 
-    let all_words = word_list_from_string(&string_to_analyze);
-    let all_syls = syllable_count_list(all_words);
-    let summed_score = linsear_scoring(all_syls);
-    let sent_count = split_into_sentences(&string_to_analyze).len() as f64;
-    // compute the score
-    let provisional_result = summed_score / sent_count;
-    // provisional adjustment 
-    let linsear_write_score: f64;
-    if provisional_result < 20.0 {
-        linsear_write_score = (provisional_result / 2.0) - 1.0
-    } else {
-        linsear_write_score = provisional_result / 2.0
-    }
-    println!("{}", linsear_write_score);
-    linsear_write_score
-}
-
-pub fn coleman_liau(file_to_analyze: &str) -> f64 {
-    //file to string
-    let string_to_analyze = file_to_string(file_to_analyze);
-    //total_words
-    let all_words = word_list_from_string(&string_to_analyze).len() as f64;
-    // characters
-    let chars = character_count(&string_to_analyze);
-    // sents
-    let sent_count = split_into_sentences(&string_to_analyze).len() as f64;
-
-    let coleman_liau_index =
-        0.0588 * ((chars / all_words) * 100.0) - 0.296 * ((sent_count / chars) * 100.0) - 15.8;
-    println!("{}", coleman_liau_index);
-    coleman_liau_index
-}
-
-pub fn ari(file_to_analyze: &str) -> f64 {
-    //file to string
-    let string_to_analyze = file_to_string(file_to_analyze);
-    //total_words
-    let all_words = word_list_from_string(&string_to_analyze).len() as f64;
-    // characters
-    let chars = character_count(&string_to_analyze);
-    // sents
-    let sent_count = split_into_sentences(&string_to_analyze).len() as f64;
-
-    let automated_readability_index =
-        (4.71 * (chars / all_words)) + (0.5 * (all_words / sent_count)) - 21.43;
-    println!("{}", automated_readability_index);
-    automated_readability_index
-}
-
-pub fn lix_string(string_to_analyze: &str) -> f64 {
+pub fn lix(string_to_analyze: &str) -> f64 {
     //total_words
     let all_words = word_list_from_string(string_to_analyze);
     //num long words
     let num_long_words = percent_long_words(all_words);
     //avg_num_words/sentence
     let sent_wordcount_list = sent_word_counts(string_to_analyze);
-    for x in &sent_wordcount_list {
-        println!("{}", x);
-    }
-    let avg_words_per_sentence = sent_avg_wordcount(sent_wordcount_list);
-    let lix_index = num_long_words + avg_words_per_sentence;
-    println!("{}", lix_index);
-    lix_index
+    let avg_words_per_sentence = sentence_average_word_count(sent_wordcount_list);
+    num_long_words + avg_words_per_sentence
 }
 
-pub fn rix_string(string_to_analyze: &str) -> f64 {
-    //total_words
+pub fn rix(string_to_analyze: &str) -> f64 {
     let all_words = word_list_from_string(string_to_analyze);
-    //num long words
-    let num_long_words = long_words(all_words);
-    // let sentences
-    let num_of_sents = split_into_sentences(string_to_analyze).len() as f64;
-    let rix_index = num_long_words / num_of_sents;
-    println!("{}", rix_index);
-    rix_index
+    let long_words_count = long_words(&all_words);
+    let sentence_count = split_into_sentences(string_to_analyze).len() as f64;
+    long_words_count / sentence_count
 }
 
-pub fn flesch_string(string_to_analyze: &str) -> f64 {
-    let sent_counts = sent_word_counts(string_to_analyze);
-    let avg_sent_length = sent_avg_wordcount(sent_counts);
-    //syls
-    let all_words = word_list_from_string(string_to_analyze);
-    let all_syls = syllable_count_list(all_words);
-    let avg_syls = avg_syl_count(all_syls);
-    let flesch_index = 206.835 - (1.015 * avg_sent_length) - (84.6 * avg_syls);
-    println!("{}", flesch_index);
-    flesch_index
-}
-
-pub fn flesch_kincaid_string(string_to_analyze: &str) -> f64 {
-    // avg_words_per_sentence
-    let sent_counts = sent_word_counts(string_to_analyze);
-    let avg_sent_length = sent_avg_wordcount(sent_counts);
-    // avg syls per word
-    let all_words = word_list_from_string(string_to_analyze);
-    let all_syls = syllable_count_list(all_words);
-    let avg_syls = avg_syl_count(all_syls);
-    let flesch_kincaid_index = (0.39 * avg_sent_length) + (11.8 * avg_syls) - 15.59;
-    println!("{}", flesch_kincaid_index);
-    flesch_kincaid_index
-}
-
-pub fn linsear_write_string(string_to_analyze: &str) -> f64{
-    //compiling variables for linsear_write 
+pub fn linsear_write(string_to_analyze: &str) -> f64 {
+    //compiling variables for linsear_write
     let all_words = word_list_from_string(string_to_analyze);
     let all_syls = syllable_count_list(all_words);
     let summed_score = linsear_scoring(all_syls);
     let sent_count = split_into_sentences(string_to_analyze).len() as f64;
+
     // compute the score
     let provisional_result = summed_score / sent_count;
-    // provisional adjustment 
-    let linsear_write_score: f64;
+
+    // provisional adjustment
     if provisional_result < 20.0 {
-        linsear_write_score = (provisional_result / 2.0) - 1.0
+        (provisional_result / 2.0) - 1.0
     } else {
-        linsear_write_score = provisional_result / 2.0
+        provisional_result / 2.0
     }
-    println!("{}", linsear_write_score);
-    linsear_write_score
 }
 
-pub fn coleman_liau_string(string_to_analyze: &str) -> f64 {
+pub fn coleman_liau(string_to_analyze: &str) -> f64 {
     //total_words
     let all_words = word_list_from_string(string_to_analyze).len() as f64;
     // characters
@@ -364,13 +188,10 @@ pub fn coleman_liau_string(string_to_analyze: &str) -> f64 {
     // sents
     let sent_count = split_into_sentences(string_to_analyze).len() as f64;
 
-    let coleman_liau_index =
-        0.0588 * ((chars / all_words) * 100.0) - 0.296 * ((sent_count / chars) * 100.0) - 15.8;
-    println!("{}", coleman_liau_index);
-    coleman_liau_index
+    0.0588 * ((chars / all_words) * 100.0) - 0.296 * ((sent_count / chars) * 100.0) - 15.8
 }
 
-pub fn automated_readability_index_string(string_to_analyze: &str) -> f64 {
+pub fn automated_readability_index(string_to_analyze: &str) -> f64 {
     //total_words
     let all_words = word_list_from_string(string_to_analyze).len() as f64;
     // characters
@@ -378,8 +199,5 @@ pub fn automated_readability_index_string(string_to_analyze: &str) -> f64 {
     // sents
     let sent_count = split_into_sentences(string_to_analyze).len() as f64;
 
-    let automated_readability_index =
-        (4.71 * (chars / all_words)) + (0.5 * (all_words / sent_count)) - 21.43;
-    println!("{}", automated_readability_index);
-    automated_readability_index
+    (4.71 * (chars / all_words)) + (0.5 * (all_words / sent_count)) - 21.43
 }
